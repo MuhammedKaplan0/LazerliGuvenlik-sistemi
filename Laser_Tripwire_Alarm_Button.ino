d0bdf5e834c2f3845bc6edc320b1f66ac53d0377
MuhammedKaplan0 <100554179+MuhammedKaplan0@users.noreply.github.com>
2022-05-30 15:01:50 +0300

    Update Laser_Tripwire_Alarm_Button.ino

--- Laser_Tripwire_Alarm_Button.ino
+++ Laser_Tripwire_Alarm_Button.ino
@@ -1,11 +1,11 @@
-int Kled=10;
+int Kled=10;  //değişkenler ve pinleri
 int Yled=12;
 int buzzer=11;
 int ldr=A4;
 
 void setup(){
-  Serial.begin(9600);
-  pinMode(Kled,OUTPUT);
+  Serial.begin(9600);   
+  pinMode(Kled,OUTPUT);   //pinleri çıkış olarak ayarlama
    pinMode(Yled,OUTPUT);
   pinMode(buzzer,OUTPUT);
 }
@@ -14,14 +14,15 @@ void loop(){
 
   int ldrdeger=analogRead(ldr);
   Serial.println(ldrdeger);
-  if(ldrdeger<500)
+  if(ldrdeger<500)              // değer 500 den küçük ise 
+
 
   {
     digitalWrite(Kled,HIGH);
-    digitalWrite(Yled,LOW);
+    digitalWrite(Yled,LOW);       // yeşil ışık söner kırmızı ışık yanar 
     tone(buzzer,30);
     delay(2000);
-    noTone (buzzer);
+    noTone (buzzer);        // buzzer belirli aralıklarda öter
     delay(500);
      tone(buzzer,30);
     
@@ -29,7 +30,7 @@ void loop(){
   else
   {
      digitalWrite(Kled,LOW);
-     digitalWrite(Yled,HIGH);
-     noTone(buzzer);
+     digitalWrite(Yled,HIGH);                  //değer 500 den küçük değil ise yeşil ışık yanar kırmızı ışık söner 
+     noTone(buzzer);                         //buzzer yanmaz
   }
 }
